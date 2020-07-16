e9b8b50c317185323aa546c9ff0490cbd48c084b
PandaChan1 <chenyang.74463@gmail.com>
2020-07-16 10:00:36 +0800

    添加点击颜色

--- caculator/app/src/main/java/com/example/caculator/MainActivity.java
+++ caculator/app/src/main/java/com/example/caculator/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.caculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+private static final String TAG="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void oneOnclick(View view){
+        Log.d(TAG,"onew be click");
+    }
+    public void onOrangeclick(View view){
+        Log.d(TAG,"onOrangeclick");
     }
 }
