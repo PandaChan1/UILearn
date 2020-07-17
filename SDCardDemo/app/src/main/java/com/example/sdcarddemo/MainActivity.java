9be46feb0252d4e8d62cb589c23497b8ec18f251
PandaChan1 <chenyang.74463@gmail.com>
2020-07-17 15:12:04 +0800

    文件路径错误，已改，但存储权限无法获得

--- SDCardDemo/app/src/main/java/com/example/sdcarddemo/MainActivity.java
+++ SDCardDemo/app/src/main/java/com/example/sdcarddemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.sdcarddemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +15,7 @@ import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG ="MainActivity" ;
     private Button mBtnWriteToSDCard;
 
     @Override
@@ -26,25 +29,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        //写数据到SD卡
-        /**
-         *
-         * SD卡读写需要在manifests文件中写明权限
-         * <uses-permission></uses-permission>该方法声明权限
-         */
-        switch (v.getId()){
-            case R.id.btn_writedatatoSDCard:
-                File filepath=new File("/storage/sdcard"); //文件路径错误，storage下无sdcard文件，待修改
-                File file=new File(filepath,"info.txt");
-                try {
-                    FileOutputStream fos=new FileOutputStream(file);
-                    fos.write("SDCard Learning Demo".getBytes());
+        if (v==mBtnWriteToSDCard) {
+            //写数据到SD卡
+            /**
+             *
+             * SD卡读写需要在manifests文件中写明权限
+             * <uses-permission></uses-permission>该方法声明权限
+             */
+            //获取sdcard路径
+            File filepath = Environment.getExternalStorageDirectory();
+            Log.d(TAG, "sdCard path==" + filepath);
+            // File filepath=new File("/storage/sdcard"); //文件路径错误，storage下无sdcard文件，待修改
+            File file = new File(filepath, "info.txt");
+            try {
+                FileOutputStream fos = new FileOutputStream(file);
+                fos.write("SDCard Learning Demo".getBytes());
 
-                    fos.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                break;
+                fos.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         }
     }
-}
+
