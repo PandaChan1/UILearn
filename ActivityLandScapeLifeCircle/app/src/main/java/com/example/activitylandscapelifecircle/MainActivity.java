5451aeb0a380f6ee1a0200ccaf82eb460b36a6fc
PandaChan1 <chenyang.74463@gmail.com>
2020-08-11 16:51:49 +0800

    横竖屏生命周期变化和对开发问题的解决方法

--- ActivityLandScapeLifeCircle/app/src/main/java/com/example/activitylandscapelifecircle/MainActivity.java
+++ ActivityLandScapeLifeCircle/app/src/main/java/com/example/activitylandscapelifecircle/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.SeekBar;
 
 /**
  * D/MainActivity: onCreate...
@@ -28,16 +30,55 @@ import android.util.Log;
  * <p/>
  * 由上，可以得出结论：
  * 当我们横竖屏切换的时候，Activity就会销毁，并且重新创建
+ *
+ *
+ * 实际开发中，可能会遇到的横竖屏切换场景
+ * 1、游戏开发
+ * 2、视频播放器
+ * 3、其他场景
+ *
+ * 横竖屏切换，Activity生命周期发生变化，对我们开发有什么影响？
+ *
+ * 如何解决呢？
+ * 1、禁止旋转，指定屏幕方向，在Manifest文件中设置Activity的screenOrientation属性
+ *
+ *
+ * 2、对配置不敏感，Manifest文件中设置Activity的configChanges属性
+ * 如 android:configChanges="keyboardHidden|screenSize|orientation"
+ * 该属性中参数表示对这些配置不敏感，即这些配置的变化不会引起Activity生命周期的变化
+ *
+ *
+ * 解决方法1，适用于只有一种屏幕状态的应用开发，，如横屏游戏开发
+ * 解决方法2，适用于两种/多种屏幕状态的应用开发，如视频播放软件
  */
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG ="MainActivity";
+    private SeekBar mProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG,"onCreate...");
+        initView();
+    }
+
+    private void initView() {
+        mProgress = findViewById(R.id.progress_bar);
+        //打印日志，发现横竖屏切换前后，mProgress是不同的两个对象
+        //那么下面的代码就会重新执行一次
+        //即若视频看到一半旋转屏幕时，seekBar就会重置为0
+        Log.d(TAG,"mProgress=="+mProgress.toString());
+        //设置初始化数据
+        mProgress.setMax(100);
+        mProgress.post(new Runnable() {
+            @Override
+            public void run() {
+                mProgress.setProgress(0);
+            }
+        });
+
     }
 
     @Override
