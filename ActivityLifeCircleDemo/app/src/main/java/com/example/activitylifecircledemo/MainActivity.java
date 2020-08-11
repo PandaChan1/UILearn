de778bc04f4ee946adbfe70d6295e321504bb688
PandaChan1 <chenyang.74463@gmail.com>
2020-08-11 09:43:54 +0800

    生命周期onCreate onDestroy 数据恢复

--- ActivityLifeCircleDemo/app/src/main/java/com/example/activitylifecircledemo/MainActivity.java
+++ ActivityLifeCircleDemo/app/src/main/java/com/example/activitylifecircledemo/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.activitylifecircledemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String MSG_RECORD ="msg_record";
+    private static final String RECORD_KEY ="record";
     private Button mSendMsg;
     private EditText mContentBox;
     private static final String TAG="MainActivity";
@@ -20,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initView();
         initListener();
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences(MSG_RECORD, MODE_PRIVATE);
+        String record = sharedPreferences.getString(RECORD_KEY, null);
+
+        if (!TextUtils.isEmpty(record)) {
+            mContentBox.setText(record);
+        }
+
     }
 
     private void initListener() {
@@ -28,8 +41,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //获取短信内容
                 String context = mContentBox.getText().toString().trim();
+                if (TextUtils.isEmpty(context)) {
+                    //判空
+                    Toast.makeText(MainActivity.this,"请输入短信内容",Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 //
-                Log.d(TAG,"发送短信....");
+                Log.d(TAG,"发送短信...."+context);
 
             }
         });
@@ -40,5 +58,17 @@ public class MainActivity extends AppCompatActivity {
         mSendMsg = findViewById(R.id.send);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        String context = mContentBox.getText().toString().trim();
+        //把数据保存到sharedPreferences里。
+        if (!TextUtils.isEmpty(context)) {
+            SharedPreferences sharedPreferences = this.getSharedPreferences(MSG_RECORD, MODE_PRIVATE);
+            SharedPreferences.Editor edit = sharedPreferences.edit();
+            edit.putString(RECORD_KEY,context);
+            edit.commit();
+        }
 
+    }
 }
