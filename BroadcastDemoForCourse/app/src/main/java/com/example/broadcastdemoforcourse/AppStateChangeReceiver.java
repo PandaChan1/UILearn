7a796b950eb35caf5c857b169bb460b63b77a897
PandaChan1 <chenyang.74463@gmail.com>
2020-08-12 17:28:00 +0800

    Execution failed for task ':app:processDebugManifest'.待修改

---
+++ BroadcastDemoForCourse/app/src/main/java/com/example/broadcastdemoforcourse/AppStateChangeReceiver.java
@@ -0,0 +1,26 @@
+package com.example.broadcastdemoforcourse;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ *
+ * 监听应用安装与卸载，主要是收集信息
+ */
+public class AppStateChangeReceiver extends BroadcastReceiver {
+    private static final String TAG ="AppStateChangeReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (Intent.ACTION_PACKAGE_ADDED.equals(action)) {
+            //应用安装
+            Log.d(TAG,"应用安装了，它的相关信息是 ——>"+intent.getData());
+        }else if (Intent.ACTION_PACKAGE_REMOVED.equals(action)){
+            //应用卸载
+            Log.d(TAG,"应用被卸载了，它的相关信息是 ——>"+intent.getData());
+        }
+    }
+}
