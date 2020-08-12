0a171a078e6a9ac339553c0f53bd979eda137782
PandaChan1 <chenyang.74463@gmail.com>
2020-08-12 15:59:25 +0800

    广播监听手机电量变化,USB充电线连接状态

--- BroadcastDemoForCourse/app/src/main/java/com/example/broadcastdemoforcourse/MainActivity.java
+++ BroadcastDemoForCourse/app/src/main/java/com/example/broadcastdemoforcourse/MainActivity.java
@@ -14,8 +14,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG ="MainActivity";
+    private static final String TAG = "MainActivity";
     private TextView mBatteryLevel;
+    private BatteryLevelReceiver mBatteryLevelReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,37 +34,60 @@ public class MainActivity extends AppCompatActivity {
     private void registerBatteryChangedLevel() {
         //第二步
         //我们要收听的频道是：电量变化
-        IntentFilter intentFilter=new IntentFilter();
+        IntentFilter intentFilter = new IntentFilter();
 
         //第三步
         //设置频道
         intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
-
+        //USB监听
+        intentFilter.addAction(Intent.ACTION_POWER_CONNECTED);
+        intentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED);
         //第四步
-        BatteryLevelReceiver batteryLevelReceiver=new BatteryLevelReceiver();
+        mBatteryLevelReceiver = new BatteryLevelReceiver();
         //第五步
         //注册广播，动态注册
         //还需要在Manifest中声明状态权限
         // This broadcast requires {@link android.Manifest.permission#BATTERY_STATS}
-        this.registerReceiver(batteryLevelReceiver,intentFilter);
+        this.registerReceiver(mBatteryLevelReceiver, intentFilter);
     }
 
 
     /**
      * 第一步，创建一个广播接收者，继承自BroadcastReceiver
      */
-    public class BatteryLevelReceiver extends BroadcastReceiver{
+    public class BatteryLevelReceiver extends BroadcastReceiver {
 
         @Override
         public void onReceive(Context context, Intent intent) {
-
             String action = intent.getAction();
-            Log.d(TAG,"收到了电量变化的广播 ：Activon is + "+action);
-            //获取当前电量信息
-            Log.d(TAG,"当前电量："+ intent.getIntExtra(BatteryManager.EXTRA_LEVEL,0));
-            if (mBatteryLevel != null) {
-                mBatteryLevel.setText("当前电量："+ intent.getIntExtra(BatteryManager.EXTRA_LEVEL,0));
+            if (Intent.ACTION_BATTERY_CHANGED.equals(action)) {
+
+                Log.d(TAG, "收到了电量变化的广播 ：Activon is + " + action);
+                int currentLevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+                //获取当前电量信息
+                Log.d(TAG, "当前电量：" + currentLevel);
+                if (mBatteryLevel != null) {
+                    mBatteryLevel.setText("当前电量：" + currentLevel);
+                }
+                int maxLevel = intent.getIntExtra(BatteryManager.EXTRA_SCALE, 0);
+                //拿到当前电量后，再除以最大值
+                float percent = currentLevel * 1.0f / maxLevel * 100;
+                Log.d(TAG, "当前电量百分比是：" + percent + "%");
+            } else if (Intent.ACTION_POWER_CONNECTED.equals(action)) {
+                Log.d(TAG, "USB连接了....");
+            } else if (Intent.ACTION_POWER_DISCONNECTED.equals(action)) {
+                Log.d(TAG, "USB断开了....");
             }
+
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //onDestroy的时候要取消广播注册，否则会导致内存泄漏
+        if (mBatteryLevelReceiver != null) {
+            this.unregisterReceiver(mBatteryLevelReceiver);
         }
     }
 }
