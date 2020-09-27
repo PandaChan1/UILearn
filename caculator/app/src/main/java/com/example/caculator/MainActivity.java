bb9cae8c3f6c2277cd33adead4a28183ecfb1851
PandaChan1 <chenyang.74463@gmail.com>
2020-09-27 14:59:19 +0800

    计算器

--- caculator/app/src/main/java/com/example/caculator/MainActivity.java
+++ caculator/app/src/main/java/com/example/caculator/MainActivity.java
@@ -7,170 +7,167 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-private static final String TAG="MainActivity";
-    private TextView mcancel;
-    private TextView mplusormanus;
-    private TextView mdengyu;
-    private TextView mjian;
-    private TextView mjia;
-    private TextView mmod;
-    private TextView mchu;
-    private TextView mnumOne;
-    private TextView mnumTwo;
-    private TextView mnumThree;
-    private TextView mnumFour;
-    private TextView mnumFive;
-    private TextView mnumSix;
-    private TextView mnumSeven;
-    private TextView mnumEight;
-    private TextView mnumNine;
-    private TextView mnumZero;
-    private TextView mcheng;
-    private TextView mdot;
-
-
+    private static final String TAG="MainActivity";
+    private TextView mBtn_cancel;
+    private TextView mBtn_plusOrminus;
+    private TextView mBtn_mod;
+    private TextView mBtn_divides;
+    private TextView mBtn_plus;
+    private TextView mBtn_minus;
+    private TextView mBtn_mult;
+    private TextView mBtn_equals;
+    private TextView mBtn_one;
+    private TextView mBtn_two;
+    private TextView mBtn_three;
+    private TextView mBtn_four;
+    private TextView mBtn_five;
+    private TextView mBtn_six;
+    private TextView mBtn_seven;
+    private TextView mBtn_eight;
+    private TextView mBtn_nine;
+    private TextView mBtn_zero;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //找到控件
+        //提取方法
         initView();
         //设置点击事件
-        initClickEvent();
-    }
-
-    private void initClickEvent() {
-        //第一种方法设置点击事件
-        mcancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG,"content=="+((TextView)v).getText().toString());
-            }
-        });
-        //第二种方法设置点击事件
-        mplusormanus.setOnClickListener(this);
-        mnumOne.setOnClickListener(this);
-        mcheng.setOnClickListener(this);
-        mchu.setOnClickListener(this);
-        mdengyu.setOnClickListener(this);
-        mdot.setOnClickListener(this);
-        mjia.setOnClickListener(this);
-        mjian.setOnClickListener(this);
-        mmod.setOnClickListener(this);
-        mnumEight.setOnClickListener(this);
-        mnumFive.setOnClickListener(this);
-        mnumFour.setOnClickListener(this);
-        mnumNine.setOnClickListener(this);
-        mnumSeven.setOnClickListener(this);
-        mnumSix.setOnClickListener(this);
-        mnumThree.setOnClickListener(this);
-        mnumTwo.setOnClickListener(this);
-        mnumZero.setOnClickListener(this);
+        initClick();
     }
 
     /**
-     * 提取方法
+     * 设置点击事件
+     */
+    private void initClick() {
+        //第一种方式
+        mBtn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG,"content == "+((TextView)v).getText().toString());
+            }
+        });
+        //第二种方式,实现接口
+        mBtn_plusOrminus.setOnClickListener(this);
+        mBtn_plus.setOnClickListener(this);
+        mBtn_mult.setOnClickListener(this);
+        mBtn_mod.setOnClickListener(this);
+        mBtn_minus.setOnClickListener(this);
+        mBtn_divides.setOnClickListener(this);
+        mBtn_equals.setOnClickListener(this);
+
+        mBtn_one.setOnClickListener(this);
+        mBtn_two.setOnClickListener(this);
+        mBtn_three.setOnClickListener(this);
+        mBtn_four.setOnClickListener(this);
+        mBtn_five.setOnClickListener(this);
+        mBtn_six.setOnClickListener(this);
+        mBtn_seven.setOnClickListener(this);
+        mBtn_eight.setOnClickListener(this);
+        mBtn_nine.setOnClickListener(this);
+        mBtn_zero.setOnClickListener(this);
+
+    }
+
+    /**
+     * 找到控件
      */
     private void initView() {
-        mcancel = findViewById(R.id.tv_cancel);
-        mplusormanus = findViewById(R.id.tv_plusormanus);
-        mmod = findViewById(R.id.tv_mod);
-        mchu = findViewById(R.id.tv_chu);
-        mnumOne = findViewById(R.id.tv_number_one);
-        mnumTwo = findViewById(R.id.tv_number_two);
-        mnumThree = findViewById(R.id.tv_number_three);
-        mnumFour = findViewById(R.id.tv_number_four);
-        mnumFive = findViewById(R.id.tv_number_five);
-        mnumSix = findViewById(R.id.tv_number_six);
-        mnumSeven = findViewById(R.id.tv_number_seven);
-        mnumEight = findViewById(R.id.tv_number_eight);
-        mnumNine = findViewById(R.id.tv_number_nine);
-        mnumZero = findViewById(R.id.tv_number_zero);
-        mcheng = findViewById(R.id.tv_cheng);
-        mdot = findViewById(R.id.tv_dot);
-        mjia = findViewById(R.id.tv_jia);
-        mjian = findViewById(R.id.tv_jian);
-        mdengyu = findViewById(R.id.tv_dengyu);
+        mBtn_cancel=this.findViewById(R.id.cancel);
+        mBtn_divides=this.findViewById(R.id.divides);
+        mBtn_plusOrminus=this.findViewById(R.id.plus_or_minus);
+        mBtn_mod=this.findViewById(R.id.mod);
+        mBtn_equals=this.findViewById(R.id.equals);
+        mBtn_plus=this.findViewById(R.id.plus);
+        mBtn_minus=this.findViewById(R.id.minus);
+        mBtn_mult=this.findViewById(R.id.mult);
+        mBtn_one=this.findViewById(R.id.one);
+        mBtn_two=this.findViewById(R.id.two);
+        mBtn_three=this.findViewById(R.id.three);
+        mBtn_four=this.findViewById(R.id.four);
+        mBtn_five=this.findViewById(R.id.five);
+        mBtn_six=this.findViewById(R.id.six);
+        mBtn_seven=this.findViewById(R.id.seven);
+        mBtn_eight=this.findViewById(R.id.eight);
+        mBtn_nine=this.findViewById(R.id.nine);
+        mBtn_zero=this.findViewById(R.id.zero);
+
     }
 
     @Override
     public void onClick(View v) {
-        /**
-         * 如果多个控件设置点击事件，可以使用switch判断
-         */
-        switch (v.getId()){
-            case R.id.tv_plusormanus:
-                Log.d(TAG,"content=="+((TextView)v).getText().toString());
+        int id = v.getId();
+        switch (id) {
+            case R.id.equals:
+                Log.d(TAG,"text == "+((TextView)v).getText().toString());
                 break;
-            case R.id.tv_number_one:
-                Log.d(TAG,"content=="+((TextView)v).getText().toString());
+            case R.id.divides:
+                Log.d(TAG,"text == "+((TextView)v).getText().toString());
                 break;
-            case R.id.tv_cheng:
-                Log.d(TAG,"content=="+((TextView)v).getText().toString());
+            case R.id.plus:
+                Log.d(TAG,"text == "+((TextView)v).getText().toString());
                 break;
-            case R.id.tv_chu:
-                Log.d(TAG,"content=="+((TextView)v).getText().toString());
+            case R.id.plus_or_minus:
+                Log.d(TAG,"text == "+((TextView)v).getText().toString());
                 break;
-            case R.id.tv_jia:
-                Log.d(TAG,"content=="+((TextView)v).getText().toString());
+            case R.id.mod:
+                Log.d(TAG,"text == "+((TextView)v).getText().toString());
                 break;
-            case R.id.tv_jian:
-                Log.d(TAG,"content=="+((TextView)v).getText().toString());
+            case R.id.mult:
+                Log.d(TAG,"text == "+((TextView)v).getText().toString());
                 break;
-            case R.id.tv_mod:
-                Log.d(TAG,"content=="+((TextView)v).getText().toString());
+            case R.id.minus:
+                Log.d(TAG,"text == "+((TextView)v).getText().toString());
                 break;
-            case R.id.tv_dot:
-                Log.d(TAG,"content=="+((TextView)v).getText().toString());
+            case R.id.one:
+                Log.d(TAG,"text == "+((TextView)v).getText().toString());
                 break;
-            case R.id.tv_dengyu:
-                Log.d(TAG,"content=="+((TextView)v).getText().toString());
+            case R.id.two:
+                Log.d(TAG,"text == "+((TextView)v).getText().toString());
                 break;
-            case R.id.tv_number_zero:
-                Log.d(TAG,"content=="+((TextView)v).getText().toString());
+            case R.id.three:
+                Log.d(TAG,"text == "+((TextView)v).getText().toString());
                 break;
-            case R.id.tv_number_two:
-                Log.d(TAG,"content=="+((TextView)v).getText().toString());
+            case R.id.four:
+                Log.d(TAG,"text == "+((TextView)v).getText().toString());
                 break;
-            case R.id.tv_number_three:
-                Log.d(TAG,"content=="+((TextView)v).getText().toString());
+            case R.id.five:
+                Log.d(TAG,"text == "+((TextView)v).getText().toString());
                 break;
-            case R.id.tv_number_four:
-                Log.d(TAG,"content=="+((TextView)v).getText().toString());
+            case R.id.six:
+                Log.d(TAG,"text == "+((TextView)v).getText().toString());
                 break;
-            case R.id.tv_number_five:
-                Log.d(TAG,"content=="+((TextView)v).getText().toString());
+            case R.id.seven:
+                Log.d(TAG,"text == "+((TextView)v).getText().toString());
                 break;
-            case R.id.tv_number_six:
-                Log.d(TAG,"content=="+((TextView)v).getText().toString());
+            case R.id.eight:
+                Log.d(TAG,"text == "+((TextView)v).getText().toString());
                 break;
-            case R.id.tv_number_seven:
-                Log.d(TAG,"content=="+((TextView)v).getText().toString());
+            case R.id.nine:
+                Log.d(TAG,"text == "+((TextView)v).getText().toString());
                 break;
-            case R.id.tv_number_eight:
-                Log.d(TAG,"content=="+((TextView)v).getText().toString());
-                break;
-            case R.id.tv_number_nine:
-                Log.d(TAG,"content=="+((TextView)v).getText().toString());
+            case R.id.zero:
+                Log.d(TAG,"text == "+((TextView)v).getText().toString());
                 break;
         }
-
     }
-//    public void oneOnclick(View view){
-//        Log.d(TAG,"onew be click");
-//        if (view instanceof TextView){
-//            String orangeclick=((TextView) view).getText().toString();
-//            Log.d(TAG, "text=="+orangeclick);
+
+
+    /*第一种方式处理点击事件，xml文件中设置onClick属性方法，在MainActivity中实现方法*/
+//    public void cancelOnClick(View view){
+//        Log.d(TAG,"cancel is click......");
+//        if (view instanceof TextView) {
+//            String s = ((TextView) view).getText().toString();
+//            Log.d(TAG,"text =="+s);
 //        }
 //    }
-//    public void onOrangeclick(View view){
-//        Log.d(TAG,"onOrangeclick");
+//    public void onOrangeClick(View view){
+//        Log.d(TAG,"onOrange click......");
 //        if (view instanceof TextView){
-//            String oneclick=((TextView) view).getText().toString();
-//            Log.d(TAG,"text=="+oneclick);
+//            String s = ((TextView) view).getText().toString();
+//            Log.d(TAG,"text =="+s);
 //        }
 //    }
+
 }
